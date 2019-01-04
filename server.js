badc8743f446296a7062297d2196da035a59b58a
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2019-01-04 19:16:10 +0700

    Start Server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('dist'));
+app.set('views', __dirname + '/dist'); // general config
+app.set('view engine', 'ejs');
+
+app.get('**', (req, res) => res.render('index'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
