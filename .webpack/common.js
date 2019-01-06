38ca8e699f1ac0f6acd72f60e8516df3d4a6e5a5
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2019-01-06 14:04:45 +0700

    Add Favicon to build using Webpack Copy Plugin

--- .webpack/common.js
+++ .webpack/common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 console.log(devMode);
@@ -9,7 +10,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'index.bundle.js',
+    filename: '[name].bundle.js',
     publicPath: '/'
   },
   module: {
@@ -76,6 +77,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new CopyWebpackPlugin([
+      // relative path is from src
+      { from: './src/assets/favicon.ico', to: './assets' }, // <- your path to favicon
+    ])
   ]
 };
