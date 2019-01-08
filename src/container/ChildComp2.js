e68951dbd82159db81f3345bfeec160530f7e03d
0xKoddoelo <0xKoddoelo@gmail.com>
2019-01-08 19:50:52 +0700

    Layout

--- src/container/ChildComp2.js
+++ src/container/ChildComp2.js
@@ -7,6 +7,7 @@ const ChildComp2 = () => (
     <p>Lazy Load By Using Suspend lazy from react 16.6</p>
     <p>Lazy Load By Using Suspend lazy from react 16.6</p>
     <p>Lazy Load By Using Suspend lazy from react 16.6</p>
+    <img src={require('../assets/img/about.jpg')} alt='Load Image' />
   </div>
 )
 
